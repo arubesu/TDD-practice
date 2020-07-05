079a4674668ea2aa01b09adebb32ce1e4725d713
Bruno Souza <bruno.arubesu@gmail.com>
2020-07-05 17:54:16 -0300

    Green: Should Convert Symbol V

--- Commom/RomanConverters/RomanConverter.cs
+++ Commom/RomanConverters/RomanConverter.cs
@@ -1,11 +1,19 @@
+using System.Collections.Generic;
+
 namespace Commom.RomanConverters
 {
   public static class RomanConverter
   {
 
+    private static Dictionary<string, int> romanDictionary =
+    new Dictionary<string, int>{
+      {"I", 1},
+      {"V", 5}
+    };
+
     public static int Convert(string symbol)
     {
-      return 1;
+      return romanDictionary[symbol];
     }
   }
 }
