78158cd9bee882c0789e5434832324aae494d726
Bruno Souza <bruno.arubesu@gmail.com>
2020-07-05 17:50:55 -0300

    Refactor, RED: Should Convert Symbol V

--- Tests/RomanConverters/RomanConvertTest.cs
+++ Tests/RomanConverters/RomanConvertTest.cs
@@ -15,6 +15,19 @@ namespace Tests.RomanConverters
     {
       var symbol = "I";
       var expected = 1;
+      ConvertAndAssertAreEqual(symbol, expected);
+    }
+
+    [Test]
+    public void ShouldConvertSymbolV()
+    {
+      var symbol = "V";
+      var expected = 5;
+      ConvertAndAssertAreEqual(symbol, expected);
+    }
+
+    private static void ConvertAndAssertAreEqual(string symbol, int expected)
+    {
       var result = RomanConverter.Convert(symbol);
       Assert.AreEqual(expected, result);
     }
