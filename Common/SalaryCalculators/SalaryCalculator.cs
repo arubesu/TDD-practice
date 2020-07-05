3e3a4b1bb28bf1fb021dd71b1a1143a14499f887
Bruno Souza <bruno.arubesu@gmail.com>
2020-07-05 20:50:10 -0300

    Refactor: Test developer discount value rule I both cases

--- Common/SalaryCalculators/SalaryCalculator.cs
+++ Common/SalaryCalculators/SalaryCalculator.cs
@@ -5,13 +5,9 @@ namespace Common.SalaryCalculators
     public static decimal Calculate(Employee employee)
     {
       if (employee.Salary > 3000)
-      {
         return employee.Salary * 0.8m;
-      }
-      else
-      {
-        return employee.Salary * 0.9m;
-      }
+
+      return employee.Salary * 0.9m;
     }
   }
 }
