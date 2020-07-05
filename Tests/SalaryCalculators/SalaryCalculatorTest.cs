e8731214a3073ad27b7c5a59ca616e829d884d0c
Bruno Souza <bruno.arubesu@gmail.com>
2020-07-05 20:31:49 -0300

    Red: Test developer discount value rule I

--- Tests/SalaryCalculators/SalaryCalculatorTest.cs
+++ Tests/SalaryCalculators/SalaryCalculatorTest.cs
@@ -9,7 +9,24 @@ namespace Tests.SalaryCalculators
     {
     }
 
+    // Developers have a 20% discount if their salary is higher than $ 3000.0.
+    // Otherwise, the discount is 10%.
 
+    [Test]
+    public void ShouldGive20PercentDiscountInDeveloperSalaryWhenSalaryIs3000()
+    {
+
+
+      Assert.Fail();
+    }
+
+
+    [Test]
+    public void ShouldGive10PercentDiscountInDeveloperSalaryWhenSalaryIsLessThan3000()
+    {
+
+      Assert.Fail();
+    }
 
   }
 }
