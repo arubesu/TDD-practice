ae24588eb1bffe3b3dea8f36ee6d13c0222cd5bb
Bruno Souza <bruno.arubesu@gmail.com>
2020-07-05 19:03:14 -0300

    Green: Should Return Negative When Symbol Repeated More Than Times In A Row Like XXXX

--- Commom/RomanConverters/RomanConverter.cs
+++ Commom/RomanConverters/RomanConverter.cs
@@ -21,19 +21,34 @@ namespace Commom.RomanConverters
     {
       var accumulator = 0;
       var symbolLength = symbol.Length;
+      var lastValue = 0;
+      var countRepeatedChar = 0;
 
       for (int i = 0; i < symbolLength; i++)
       {
-
         var currentValue = romanDictionary[symbol[i]];
         var nextIndex = i + 1 > symbolLength - 1 ? i : i + 1;
         var nextValue = romanDictionary[symbol[nextIndex]];
         var multiplier = 1;
 
+        if (i == 0 || lastValue == currentValue)
+        {
+          countRepeatedChar++;
+        }
+        else
+        {
+          countRepeatedChar = 1;
+        }
+
+        if (countRepeatedChar > 3)
+          return -1;
+
         if (nextValue > currentValue)
           multiplier = -1;
 
         accumulator += currentValue * multiplier;
+
+        lastValue = currentValue;
       }
 
       return accumulator;
